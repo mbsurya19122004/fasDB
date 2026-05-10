7bf3a3b40ff7c5fe3b6bade0cb3641bba5764bae
mbsurya19122004 <mbsurya19122004@gmail.com>
2026-05-11 00:58:25 +0530

    read function for reads from  hashmap

--- src/functions.rs
+++ src/functions.rs
@@ -4,3 +4,11 @@ pub fn write(key : String , val : String){
     storage::MAIN_STORAGE.lock().unwrap()
     .insert(key,val);
 }
+
+pub fn read(key : String) -> Option<String> {
+    match storage::MAIN_STORAGE.lock().unwrap().get(&key){
+        Some(val) => return Some(val.to_string()), //Might cause Memory issues later .... need to check later
+        None => None, //Change later 
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,18 @@ fn main() {
     functions::write("key1".to_string(),"val1".to_string());
     functions::write("key2".to_string(),"val1".to_string());
     functions::write("key3".to_string(),"val1".to_string());
-    functions::write("key4".to_string(),"val1".to_string());    
+    functions::write("key4".to_string(),"val1".to_string());  
+    let x = functions::read("key$".to_string());  
+    let y = functions::read("key4".to_string());
+
     println!("{:?}",storage::MAIN_STORAGE.lock().unwrap());
+    match x{
+        Some(val) => println!("{}",val),
+        None => println!("NOT Found"),
+    }
+        match y{
+        Some(val) => println!("{}",val),
+        None => println!("NOT Found"),
+    }    
 }
 
